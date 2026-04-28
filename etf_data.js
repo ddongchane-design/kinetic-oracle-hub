8bb8999960fdde24af1c0c4ce545523aded99f19
Graph Admin <knowledge@graph.com>
2026-04-28 10:51:51 +0900

    feat: Add related ETF recommendations to all workspace and insight cards

---
+++ etf_data.js
@@ -0,0 +1,32 @@
+window.ETF_DB = {
+  "bmnr_graph.html": [
+    { ticker: "375290", name: "KBSTAR 글로벌블록체인", category: "블록체인" },
+    { ticker: "138230", name: "KODEX 달러선물", category: "달러" },
+    { ticker: "133690", name: "TIGER 미국나스닥100", category: "나스닥" },
+  ],
+  "iran_graph.html": [
+    { ticker: "261220", name: "TIGER 원유선물(H)", category: "원유" },
+    { ticker: "411060", name: "ACE KRX금현물", category: "금현물" },
+    { ticker: "132030", name: "KODEX 골드선물(H)", category: "금선물" },
+  ],
+  "space_graph.html": [
+    { ticker: "305720", name: "TIGER 우주항공&UAM", category: "우주" },
+    { ticker: "091160", name: "KODEX 반도체", category: "반도체" },
+    { ticker: "133690", name: "TIGER 미국나스닥100", category: "나스닥" },
+  ],
+  "tempus_graph.html": [
+    { ticker: "244580", name: "KODEX 바이오", category: "바이오" },
+    { ticker: "232080", name: "TIGER 코스닥150바이오테크", category: "바이오" },
+    { ticker: "133690", name: "TIGER 미국나스닥100", category: "나스닥" },
+  ],
+  "hyundai_graph.html": [
+    { ticker: "244580", name: "KODEX 바이오", category: "바이오" },
+    { ticker: "232080", name: "TIGER 코스닥150바이오테크", category: "바이오" },
+    { ticker: "069500", name: "KODEX 200", category: "KOSPI" },
+  ],
+  "ai_graph.html": [
+    { ticker: "091160", name: "KODEX 반도체", category: "반도체" },
+    { ticker: "091230", name: "TIGER 반도체", category: "반도체" },
+    { ticker: "379800", name: "KODEX 미국나스닥100", category: "나스닥" },
+  ],
+};
